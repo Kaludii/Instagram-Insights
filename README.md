4d1408466eece9db94122aa9ed0222df83052a9d
Bilal Kaludi <63890666+Kaludii@users.noreply.github.com>
2023-03-21 21:35:18 -0500

    adding updated files for the Instagram Insights Project

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Instagram-Insights
+
+# Instagram Insights
+
 Instagram Insights is a Streamlit application that allows you to analyze and visualize your Instagram data like users not following you back, users you aren't following back, and more.
+
+# Web App
+Click [Here](https://huggingface.co/spaces/Kaludi/Instagram-Insights-Streamlit_App "Here") To View This App Online!
+
+## Features
+
+-   Visualize insights from your Instagram data like followers, following, follow requests, and more.
+-   Filter and download the data for further analysis.
+-   Easy to use interface.
+
+## Usage
+
+1.  Download your Instagram data by going to **Settings > Data Download**, and click on "Request Download". Make sure you download the data as a JSON file.
+    
+2.  When the data is emailed to you, upload all the files in the `followers_and_following` folder to the app.
+    
+3.  Use the filters in the sidebar to view and analyze different aspects of your Instagram data.
+    
+4.  Download the filtered data as CSV files for further analysis.
+
+## Getting Started
+
+### Prerequisites
+
+-   Python 3.7 or higher
+-   Streamlit
+-   Pandas
+-   Plotly
+
+To install the required libraries, run the following command:
+
+`pip install streamlit pandas plotly` 
+
+### How to Run
+
+1.  Clone this repository.
+
+`git clone https://github.com/Kaludii/Instagram-Insights.git` 
+
+2.  Navigate to the project directory.
+
+`cd Instagram-Insights` 
+
+3.  Run the Streamlit app.
+
+`streamlit run app.py` 
+
+4.  Open your web browser and visit `http://localhost:8501`.
---
+++ app.py
@@ -0,0 +1,166 @@
+import streamlit as st
+import pandas as pd
+import json
+import plotly.express as px
+from datetime import datetime
+import plotly.graph_objs as go
+import base64
+from io import StringIO
+
+import base64
+
+def generate_download_link(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    button_style = (
+        "background-color: #4CAF50;"
+        "border: none;"
+        "color: white;"
+        "padding: 10px 20px;"
+        "text-align: center;"
+        "text-decoration: none;"
+        "display: inline-block;"
+        "font-size: 16px;"
+        "margin: 4px 2px;"
+        "cursor: pointer;"
+        "border-radius: 8px;"
+    )
+    return f'<a href="data:file/csv;base64,{b64}" download="{filename}" style="{button_style}">Download {filename}</a>'
+
+
+def count_users(json_data, key):
+    return len(json_data[key])
+
+def get_users(json_data, key):
+    users = []
+    for item in json_data[key]:
+        user_data = item['string_list_data'][0]
+        timestamp = datetime.utcfromtimestamp(user_data['timestamp']).strftime('%m/%d/%Y %H:%M')
+        users.append({'Username': user_data['value'], 'Profile URL': user_data['href'], 'Timestamp': timestamp})
+    return users
+
+st.set_page_config(page_title='Instagram Insights')
+st.title('Instagram Insights')
+
+st.markdown('''
+Welcome to Instagram Insights, a tool to help you analyze and understand your Instagram data like users not following you back or users you aren't following back.
+Upload your Instagram data, and the app will visualize various insights such as followers, following, follow requests, and more.
+Filter and download the data for further analysis. You can download your Instagram data by going to Settings > Data Download, and click on 'Request Download'.
+Make sure you download the data as a JSON file. When the data is emailed to you, upload all the files in the 'followers_and_following' folder, and that's it!
+''')
+
+
+uploaded_files = st.file_uploader('Upload your Instagram folder', type=['json'], accept_multiple_files=True)
+
+if uploaded_files:
+    try:
+        st.sidebar.title('Filters')
+        users_not_following_me_back = st.sidebar.checkbox('Users Not Following Me Back')
+        users_im_not_following_back = st.sidebar.checkbox("Users I'm Not Following Back")
+
+        data = {}
+        for file in uploaded_files:
+            file_name = file.name
+            file_content = file.getvalue().decode()
+            data[file_name] = json.loads(file_content)
+        
+        followers = get_users(data['followers.json'], 'relationships_followers')
+        following = get_users(data['following.json'], 'relationships_following')
+
+        # Create bar chart with counts
+        chart_labels = [
+            'Followers',
+            'Following',
+            "Follow Requests Received",
+            "Pending Follow Requests",
+            "Recent Follow Requests",
+            "Recently Unfollowed Accounts",
+        ]
+
+        chart_values = [
+            count_users(data['followers.json'], 'relationships_followers'),
+            count_users(data['following.json'], 'relationships_following'),
+            count_users(data["follow_requests_you've_received.json"], 'relationships_follow_requests_received'),
+            count_users(data['pending_follow_requests.json'], 'relationships_follow_requests_sent'),
+            count_users(data['recent_follow_requests.json'], 'relationships_permanent_follow_requests'),
+            count_users(data['recently_unfollowed_accounts.json'], 'relationships_unfollowed_users'),
+        ]
+
+        bar_chart = go.Figure(
+            data=[
+                go.Bar(x=chart_labels, y=chart_values, text=chart_values, textposition='auto')
+            ]
+        )
+
+        bar_chart.update_layout(
+            title="Instagram Insights Summary",
+            xaxis_title="Categories",
+            yaxis_title="Count",
+            plot_bgcolor="rgba(0, 0, 0, 0)",
+        )
+
+        st.plotly_chart(bar_chart)
+        
+        if users_not_following_me_back:
+            not_following_me_back = [user for user in following if user['Username'] not in [follower['Username'] for follower in followers]]
+            df_not_following_me_back = pd.DataFrame(not_following_me_back)
+            st.subheader(f"Users Not Following Me Back ({len(df_not_following_me_back) - 1})")
+            st.write(df_not_following_me_back)
+            st.markdown(generate_download_link(df_not_following_me_back, "users_not_following_me_back.csv"), unsafe_allow_html=True)
+
+        if users_im_not_following_back:
+            im_not_following_back = [user for user in followers if user['Username'] not in [following_user['Username'] for following_user in following]]
+            df_im_not_following_back = pd.DataFrame(im_not_following_back)
+            st.subheader(f"Users I'm Not Following Back ({len(df_im_not_following_back) - 1})")
+            st.write(df_im_not_following_back)
+            st.markdown(generate_download_link(df_im_not_following_back, "users_im_not_following_back.csv"), unsafe_allow_html=True)
+
+
+        # Add 'Files Filter' section to the sidebar
+        st.sidebar.title('Files Filter')
+        show_followers = st.sidebar.checkbox('Show Followers')
+        show_following = st.sidebar.checkbox('Show Following')
+        show_received_requests = st.sidebar.checkbox("Show Follow Requests Received")
+        show_pending_requests = st.sidebar.checkbox("Show Pending Follow Requests")
+        show_recent_requests = st.sidebar.checkbox("Show Recent Follow Requests")
+        show_unfollowed_accounts = st.sidebar.checkbox("Show Recently Unfollowed Accounts")
+
+        # Display the corresponding dataframes based on the checkboxes
+        if show_followers:
+            st.subheader('Followers')
+            st.write(pd.DataFrame(followers))
+            st.markdown(generate_download_link(pd.DataFrame(followers), "followers.csv"), unsafe_allow_html=True)
+
+        if show_following:
+            st.subheader('Following')
+            st.write(pd.DataFrame(following))
+            st.markdown(generate_download_link(pd.DataFrame(following), "following.csv"), unsafe_allow_html=True)
+
+        if show_received_requests:
+            received_requests = get_users(data["follow_requests_you've_received.json"], 'relationships_follow_requests_received')
+            st.subheader("Follow Requests Received")
+            st.write(pd.DataFrame(received_requests))
+            st.markdown(generate_download_link(pd.DataFrame(received_requests), "follow_requests_received.csv"), unsafe_allow_html=True)
+
+        if show_pending_requests:
+            pending_requests = get_users(data['pending_follow_requests.json'], 'relationships_follow_requests_sent')
+            st.subheader("Pending Follow Requests")
+            st.write(pd.DataFrame(pending_requests))
+            st.markdown(generate_download_link(pd.DataFrame(pending_requests), "pending_follow_requests.csv"), unsafe_allow_html=True)
+
+        if show_recent_requests:
+            recent_requests = get_users(data['recent_follow_requests.json'], 'relationships_permanent_follow_requests')
+            st.subheader("Recent Follow Requests")
+            st.write(pd.DataFrame(recent_requests))
+            st.markdown(generate_download_link(pd.DataFrame(recent_requests), "recent_follow_requests.csv"), unsafe_allow_html=True)
+
+        if show_unfollowed_accounts:
+            unfollowed_accounts = get_users(data['recently_unfollowed_accounts.json'], 'relationships_unfollowed_users')
+            st.subheader("Recently Unfollowed Accounts")
+            st.write(pd.DataFrame(unfollowed_accounts))
+            st.markdown(generate_download_link(pd.DataFrame(unfollowed_accounts), "recently_unfollowed_accounts.csv"), unsafe_allow_html=True)
+
+    except KeyError:
+        st.error("Please make sure to upload all required files.")
+else:
+    st.warning("Please upload your Instagram data files.")
